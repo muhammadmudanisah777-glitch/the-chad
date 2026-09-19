15e258548fd382866fc350978313bca9f72699a1
muhammadmudanisah777-glitch <muhammadmudanisah777@gmail.com>
2026-09-19 06:54:18 +0100

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -1,5 +1,5 @@
 // ============================================
-//SolbixSniper TRADING BOT - Complete Implementation
+//Tradewiz TRADING BOT - Complete Implementation
 // with Debug Logging & Professional Messages 
 // ============================================
 import { Telegraf, Markup } from 'telegraf';
